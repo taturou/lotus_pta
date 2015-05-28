ea7c5fe949b30ba8fd68c1cf51af7df0d43f17bc
taturou <taturou@gmail.com>
2015-05-28 17:34:26 +0900

    Controller、View、Templateでデータを受け渡し

--- apps/announce/controllers/home/index.rb
+++ apps/announce/controllers/home/index.rb
@@ -2,7 +2,10 @@ module Announce::Controllers::Home
   class Index
     include Announce::Action
 
+    expose :time
+
     def call(params)
+      @time = Time.now
     end
   end
 end
--- apps/announce/views/home/index.rb
+++ apps/announce/views/home/index.rb
@@ -1,5 +1,9 @@
 module Announce::Views::Home
   class Index
     include Announce::View
+
+    def hoge
+      time.strftime("%Y-%m-%d %a %H:%M:%S")
+    end
   end
 end
