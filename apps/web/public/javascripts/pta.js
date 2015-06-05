476fcf1ecd33474ac1b7fbfd7fb62e6fde9938cf
taturou <taturou@gmail.com>
2015-06-05 21:19:57 +0900

    テキストエリアの高さを本文の行数に合わせて拡大するように

--- apps/web/public/javascripts/pta.js
+++ apps/web/public/javascripts/pta.js
@@ -1,4 +1,5 @@
 $(function() {
+  // フォームで DELETE、PUT、PATCH を送信する
   $('form').submit(function(event) {
     event.preventDefault();
 
@@ -27,5 +28,36 @@ $(function() {
           location = $redirect_url;
       },
     });
+  });
+
+  // textarea の自動拡大
+  $("textarea").on("input", function(event) {
+    if(event.target.scrollHeight > event.target.offsetHeight) {
+      $(event.target).height(event.target.scrollHeight);
+    } else {
+      var lineHeight = Number($(event.target).css("line-height").split("px")[0]);
+      while (true) {
+        $(event.target).height($(event.target).height() - lineHeight);
+        if(event.target.scrollHeight > event.target.offsetHeight) {
+          $(event.target).height(event.target.scrollHeight);
+          break;
+        }
+      }
+    }
+  });
+
+  $("textarea").on("click", function(event) {
+    if(event.target.scrollHeight > event.target.offsetHeight) {
+      $(event.target).height(event.target.scrollHeight);
+    } else {
+      var lineHeight = Number($(event.target).css("line-height").split("px")[0]);
+      while (true) {
+        $(event.target).height($(event.target).height() - lineHeight);
+        if(event.target.scrollHeight > event.target.offsetHeight) {
+          $(event.target).height(event.target.scrollHeight);
+          break;
+        }
+      }
+    }
   });
 });
