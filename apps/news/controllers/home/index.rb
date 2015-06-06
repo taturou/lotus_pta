2f076c65399b6ec42ef80a1aea98a16355f9d9e1
taturou <taturou@gmail.com>
2015-06-06 23:37:39 +0900

    連絡事項の確認ボタンが機能するように

--- apps/news/controllers/home/index.rb
+++ apps/news/controllers/home/index.rb
@@ -4,7 +4,7 @@ module News::Controllers::Home
 
     expose :article, :user
 
-    def save_log(params, user, article)
+    def log_opened(params, user, article)
       http_client_ip = params.env['HTTP_X_FORWARDED_FOR'] || params.env['REMOTE_ADDR']
       http_user_agent = params.env['HTTP_USER_AGENT']
       log = ::Log.new({kind: Log::KIND_OPEN,
@@ -22,7 +22,34 @@ module News::Controllers::Home
 
       @article = spectator.article
       @user = spectator.user
-      save_log(params, @user, @article)
+      log_opened(params, @user, @article)
+    end
+  end
+
+  class Submit
+    include News::Action
+
+    expose :article, :user
+
+    def log_submitted(params, user, article)
+      http_client_ip = params.env['HTTP_X_FORWARDED_FOR'] || params.env['REMOTE_ADDR']
+      http_user_agent = params.env['HTTP_USER_AGENT']
+      log = ::Log.new({kind: Log::KIND_SUBMIT,
+                       http_client_ip: http_client_ip,
+                       http_user_agent: http_user_agent})
+      log.user = user
+      log.article = article
+      log.accessed_at = Time.now
+      LogRepository.persist(log)
+    end
+
+    def call(params)
+      spectator = SpectatorRepository.find_by_md5(params['md5'])
+      halt 404 unless spectator
+
+      @article = spectator.article
+      @user = spectator.user
+      log_submitted(params, @user, @article)
     end
   end
 end
--- apps/news/views/home/index.rb
+++ apps/news/views/home/index.rb
@@ -2,5 +2,24 @@ module News::Views::Home
   class Show
     include News::View
 
+    def logs_as_article(user, article)
+      logs = article.logs.clone
+      logs.delete_if do |log|
+        log.user.id != user.id
+      end
+      logs
+    end
+
+    def submitted?
+      logs = logs_as_article(user, article)
+      logs.delete_if do |log|
+        log.kind != Log::KIND_SUBMIT
+      end
+      logs.length > 0
+    end
+  end
+
+  class Submit
+    include News::View
   end
 end
