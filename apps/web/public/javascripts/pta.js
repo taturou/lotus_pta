decad8f458ed105e68799f06c5ef85061862d121
taturou <taturou@gmail.com>
2015-06-12 17:32:05 +0900

    連絡事項の削除ボタンを押したとき確認のダイアログを出す refs #26

--- apps/web/public/javascripts/pta.js
+++ apps/web/public/javascripts/pta.js
@@ -1,13 +1,13 @@
 $(function() {
   // 設定のプルダウンメニュー
-  $("#config-menu").children("#button").on("click", function(event) {
-    $(this).parent("#config-menu").children("#background").toggleClass("hide")
-    $(this).parent("#config-menu").children("#content").toggleClass("hide")
+  $('#config-menu').children('#button').on('click', function(event) {
+    $(this).parent('#config-menu').children('#background').toggleClass('hide')
+    $(this).parent('#config-menu').children('#content').toggleClass('hide')
   });
 
-  $("#config-menu").children("#background").on("click", function(event) {
-    $(this).toggleClass("hide")
-    $(this).parent("#config-menu").children("#content").toggleClass("hide")
+  $('#config-menu').children('#background').on('click', function(event) {
+    $(this).toggleClass('hide')
+    $(this).parent('#config-menu').children('#content').toggleClass('hide')
   });
 
   // ボタンをタップしたときのスタイル設定
@@ -41,11 +41,11 @@ $(function() {
   });
 
   // textarea の自動拡大
-  $("textarea").on("input click", function(event) {
+  $('textarea').on('input click', function(event) {
     if(event.target.scrollHeight > event.target.offsetHeight) {
       $(event.target).height(event.target.scrollHeight);
     } else {
-      var lineHeight = Number($(event.target).css("line-height").split("px")[0]);
+      var lineHeight = Number($(event.target).css('line-height').split('px')[0]);
       while (true) {
         $(event.target).height($(event.target).height() - lineHeight);
         if(event.target.scrollHeight > event.target.offsetHeight) {
@@ -54,5 +54,66 @@ $(function() {
         }
       }
     }
+  });
+
+  // alert
+  $('#alert').on('click', function(event) {
+    // ダイアログに表示するデータを取得
+    var data = {};
+    $(this).children('input').forEach(function(item, index, array) {
+      data[item.name] = item.value;
+    });
+
+    // 元のコンテンツを固定する
+    var org_overflow = $('html').css('overflow');
+    var org_height = $('html').css('height');
+    $('html').css('overflow', 'hidden');
+    $('html').css('height', '100%');
+    $(document).bind("touchmove", function(event) {
+      event.preventDefault();
+    });
+
+    // バックグラウンドを作成
+    $('body').append('<div id="alert_background" class="fixed top-0 right-0 bottom-0 left-0 z4 bg-black muted"></div>');
+
+    // ダイアログを作成
+    $('body').append(
+      '<div id="alert_dialog" class="fixed top-0 right-0 bottom-0 left-0 z4 bg-white mt4 mb4 ml2 mr2 border rounded flex flex-column">'
+     +  '<main class="flex-auto center p2">'
+     +    data['alert_content']
+     +  '</main>'
+     +  '<footer class="flex">'
+     +    '<form action="' + data['alert_action'] + '", method="post" id="alert_submit" class="col-6 border-top border-right center p0">'
+     +      '<input name="_method", value="' + data['alert_method'] + '", type="hidden">'
+     +      '<button type="submit" class="col-12 button button-transparent center p2 blue m0">'
+     +        data['alert_button_submit']
+     +      '</button>'
+     +    '</form>'
+     +    '<div id="alert_cancel" class="col-6 button button-transparent border-top p2 center blue">'
+     +      data['alert_button_cancel']
+     +    '</div>'
+     +  '</footer>'
+     +'</div>'
+      );
+
+    // バックグラウンドをクリックしたら全てを削除する
+    fn_alert_delete = function(event) {
+      $("#alert_background").remove();
+      $('#alert_dialog').remove();
+      $('html').css('overflow', org_overflow);
+      $('html').css('height', org_height);
+      $(document).unbind();
+    }
+    $('#alert_background').on('click', fn_alert_delete);
+    $('#alert_cancel').on('click', fn_alert_delete);
+
+    // ボタンをタップしたときのスタイル設定
+    $('.button-transparent, .button-outline').on('touchstart touchend', function(event) {
+      if (event.type == 'touchstart') {
+        $(this).addClass('tap');
+      } else {
+        $(this).removeClass('tap');
+      }
+    });
   });
 });
--- apps/web/views/articles/show.rb
+++ apps/web/views/articles/show.rb
@@ -14,14 +14,14 @@ module Web::Views::Articles
     def edit_icon
       if readed? || article.owner != login_user
         html.div(class: 'button button-outline right gray mr1') do
-          i(class: 'fa fa-edit'){}
-          span(){ t('form.edit') }
+          i(class: 'fa fa-edit')
+          span{ t('form.edit') }
         end
       else
         html.a(href: routes.path(:edit_article, id: article.id),
                class: 'button button-outline right blue mr1') do
-          i(class: 'fa fa-edit'){}
-          span(){ t('form.edit') }
+          i(class: 'fa fa-edit')
+          span{ t('form.edit') }
         end
       end
     end
@@ -29,34 +29,37 @@ module Web::Views::Articles
     def delete_icon
       if readed? || article.owner != login_user
         html.div(class: 'button button-outline right gray') do
-          i(class: 'fa fa-trash-o'){}
-          span(){ t('form.delete') }
+          i(class: 'fa fa-trash-o')
+          span{ t('form.delete') }
         end
       else
-        html.form(action: routes.path(:article, id: article.id), method: 'post') do
-          input(name: '_method', type: 'hidden', value: 'delete'){}
-          button(type: 'submit', class: 'button button-outline right blue') do
-            i(class: 'fa fa-trash-o'){}
-            span(){ t('form.delete') }
-          end
+        path = routes.path(:article, id: article.id)
+        html.div(class: 'button button-outline right blue', id: 'alert') do
+          i(class: 'fa fa-trash-o')
+          span{ t('form.delete') }
+          input(name: 'alert_method', value: 'delete', type: 'hidden')
+          input(name: 'alert_action', value: path, type: 'hidden')
+          input(name: 'alert_content', value: t('article.deldlg_content'), type: 'hidden')
+          input(name: 'alert_button_submit', value: t('article.deldlg_submit'), type: 'hidden')
+          input(name: 'alert_button_cancel', value: t('article.deldlg_cancel'), type: 'hidden')
         end
       end
     end
 
     def read_icon(reader)
       if submitted?(reader)
-        html.i(class: 'fa fa-check-square-o'){}
+        html.i(class: 'fa fa-check-square-o')
       elsif opened?(reader)
-        html.i(class: 'fa fa-folder-open-o'){}
+        html.i(class: 'fa fa-folder-open-o')
       else
-        html.i(class: 'fa fa-folder-o'){}
+        html.i(class: 'fa fa-folder-o')
       end
     end
 
     def read_date(reader)
       unless opened?(reader)
         html.a(href: routes.path(:link_article, id: article.id, reader_user_id: reader.id)) do
-          i(class: 'fa fa-share'){}
+          i(class: 'fa fa-share')
         end
       else
         accessed_at(reader)
