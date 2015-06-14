4f82b64b894b6a99893cc325d37df1e0fc9f09ac
taturou <taturou@gmail.com>
2015-06-14 21:26:23 +0900

    $ lotus new pta --database=sqlite3 --application=admin --application-base-url=admin --path=.
    conflict したファイルは全部 Overwrite しない。
    そうすると追記してくれる。
    
    $ ./lotus new pta --database=sqlite3 --application=admin --application-base-url=admin --path=.
       identical  .lotusrc
        conflict  Gemfile
    Overwrite /lotus_pta/Gemfile? (enter "h" for help) [Ynaqdh] n
            skip  Gemfile
       identical  config.ru
        conflict  config/environment.rb
    Overwrite /lotus_pta/config/environment.rb? (enter "h" for help) [Ynaqdh] n
            skip  config/environment.rb
       identical  config/.env
        conflict  config/.env.development
    Overwrite /lotus_pta/config/.env.development? (enter "h" for help) [Ynaqdh] n
            skip  config/.env.development
        conflict  config/.env.test
    Overwrite /lotus_pta/config/.env.test? (enter "h" for help) [Ynaqdh] n
            skip  config/.env.test
        conflict  lib/pta.rb
    Overwrite /lotus_pta/lib/pta.rb? (enter "h" for help) [Ynaqdh] n
            skip  lib/pta.rb
        conflict  lib/config/mapping.rb
    Overwrite /lotus_pta/lib/config/mapping.rb? (enter "h" for help) [Ynaqdh] n
            skip  lib/config/mapping.rb
        conflict  Rakefile
    Overwrite /lotus_pta/Rakefile? (enter "h" for help) [Ynaqdh] n
            skip  Rakefile
       identical  spec/spec_helper.rb
       identical  spec/features_helper.rb
       identical  db/.gitkeep
       identical  lib/pta/entities/.gitkeep
       identical  lib/pta/repositories/.gitkeep
       identical  spec/pta/entities/.gitkeep
       identical  spec/pta/repositories/.gitkeep
       identical  spec/support/.gitkeep
        conflict  .gitignore
    Overwrite /lotus_pta/.gitignore? (enter "h" for help) [Ynaqdh] n
            skip  .gitignore
             run  git init /lotus_pta from "."
          insert  config/environment.rb
          insert  config/environment.rb
          append  config/.env.development
          append  config/.env.development
          append  config/.env.test
          append  config/.env.test
          create  apps/admin/application.rb
          create  apps/admin/config/routes.rb
          create  apps/admin/config/mapping.rb
          create  apps/admin/controllers/home/index.rb
          create  apps/admin/views/application_layout.rb
          create  apps/admin/templates/application.html.erb
          create  apps/admin/views/home/index.rb
          create  apps/admin/templates/home/index.html.erb
          create  apps/admin/public/javascripts/.gitkeep
          create  apps/admin/public/stylesheets/.gitkeep
          create  spec/admin/features/.gitkeep
          create  spec/admin/controllers/.gitkeep
          create  spec/admin/views/.gitkeep
    $

--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'lotus/setup'
 require_relative '../lib/pta'
+require_relative '../apps/admin/application'
 require_relative '../apps/web/application'
 require_relative '../apps/news/application'
 
 Lotus::Container.configure do
+  mount Admin::Application, at: 'admin'
   mount News::Application, at: '/news'
   mount Web::Application, at: '/'
 end
